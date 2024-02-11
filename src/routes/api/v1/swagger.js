9c9aca02a5a7b5d4a35a59e1ac6a5aec7623efb1
erickdek <esgomez2@espe.edu.ec>
2024-02-11 14:42:54 -0500

    swagger fix try

--- src/routes/api/v1/swagger.js
+++ src/routes/api/v1/swagger.js
@@ -6,23 +6,22 @@ import path from 'path';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 //Info about the Api
-const apiOptions = {
+const apiDocsV1 = {
     definition: {
-        openapi: "3.0.0",
-        info: {title: "Arneby API", version: "1.0.0"}
+        openapi: "3.0.1",
+        info: {title: "Arneby API v1", version: "1.0.0"}
     },
     apis: [path.join(__dirname, "../v1/*.js")]
 }
 
-//DOCS en json format
-const swaggerSpec = SwaggerJSDoc(apiOptions);
+const specsV1 = SwaggerJSDoc(apiDocsV1);
 
 //Function to setup our docs
 export const swaggerDocs = (app, port) => {
-    app.use('/v1/docs', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec));
+    app.use('/v1/docs', SwaggerUI.serve, SwaggerUI.setup(specsV1));
     app.get('/v1/docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec);
+        res.send(specsV1);
     });
 
     console.log(`Doc api/v1/docs is available at http://localhost:${port}/v1/docs`);
